3a3387647930814ca79ecab1ff1b1bf4b924af30
jake8440 <jake8440@naver.com>
2022-08-05 01:02:02 +0900

    2022.08.05 01:01 - 최초 Commit, 프로젝트 최초 생성

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        String str1 = "cat";
+        String str2 = "cat";
+
+        System.out.println(str1 == str2);
+
+    }
+}
